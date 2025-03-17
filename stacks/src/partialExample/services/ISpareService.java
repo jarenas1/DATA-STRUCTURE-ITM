1ceb2b075d47613b7e423babd5c5b4267dd22c11
Sala K 302 <salak302@salasfr.itm.edu.co>
2025-03-17 07:32:26 -0500

    create: menu

--- stacks/src/partialExample/services/ISpareService.java
+++ stacks/src/partialExample/services/ISpareService.java
@@ -6,10 +6,9 @@ import java.math.BigDecimal;
 import java.util.Stack;
 
 public interface ISpareService {
-    Spare findSpare(Stack<Spare> spareStack, String reference);
-    Spare addSpare(Stack<Spare> spareStack, Spare spare);
-    void createSpare(String brand, String reference, Long cuantity, BigDecimal price);
-    Spare deleteSpare(Stack<Spare> spareStack, String reference);
+    void findSpare(Stack<Spare> spareStack, String reference);
+    Stack<Spare> createSpare(Stack<Spare> spareStack);
+    void deleteSpare(Stack<Spare> spareStack, String reference);
     Spare sellSpare(Stack<Spare> spareStack, String reference, Long units);
 
 
--- stacks/src/partialExample/services/SpareServiceImpl.java
+++ stacks/src/partialExample/services/SpareServiceImpl.java
@@ -2,6 +2,7 @@ package partialExample.services;
 
 import partialExample.entities.Spare;
 
+import java.awt.*;
 import java.math.BigDecimal;
 import java.sql.SQLOutput;
 import java.util.Objects;
@@ -10,13 +11,63 @@ import java.util.Stack;
 
 public class SpareServiceImpl implements ISpareService{
 
-    Scanner scanner = new Scanner(System.in);
 
     public void createStack(){
         Stack<Spare> spareStack = new Stack<>();
-        boolean stopper = true;
         System.out.println("LLENAR INVENTARIO:");
-        while(stopper){
+        Stack<Spare> filledStack = createSpare(spareStack);
+        menu(filledStack);
+        }
+
+
+    public void menu (Stack<Spare> spareStack){
+        boolean bandera = true;
+        Scanner sc = new Scanner(System.in);
+        while(bandera){
+            System.out.println("Ingrese una opcion:");
+            System.out.println("1: Buscar respuesto");
+            System.out.println("2: Crear respuesto");
+            System.out.println("3: Eliminar respuesto");
+            System.out.println("4: Vender respuesto");
+            System.out.println("4: Salir");
+            int option = sc.nextInt();
+            sc.nextLine();
+
+            switch (option){
+                case 1:
+                    System.out.println("Ingrese la referencia del repuesto");
+                    String spareReference = sc.nextLine();
+                    findSpare(spareStack, spareReference);
+                case 2:
+                    createSpare(spareStack);
+                case 3:
+                    System.out.println("Ingrese la referencia del repuesto");
+                    String spareReferenc = sc.nextLine();
+                    deleteSpare(spareStack, spareReferenc);
+                case 4:
+
+            }
+        }
+    }
+
+
+
+    @Override
+    public void findSpare(Stack<Spare> spareStack, String reference) {
+        Spare spareFounded = spareStack.stream()
+                .map(spare-> spare.getReference().equals(reference) ? spare : null)
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElse(null);
+
+    }
+
+
+    @Override
+    public Stack<Spare>  createSpare(Stack<Spare> spareStack) {
+        Scanner scanner = new Scanner(System.in);
+        boolean stopper = true;
+        while (stopper) {
             System.out.println("Ingrese la marca del respuesto:");
             String brand = scanner.nextLine();
             System.out.println("Ingrese la referencia del respuesto");
@@ -34,44 +85,37 @@ public class SpareServiceImpl implements ISpareService{
             System.out.println("Desea añadir otro item? 1: si, 2: no");
             int option = scanner.nextInt();
             scanner.nextLine();
-            if (option == 2){
+            if (option == 2) {
                 stopper = false;
             }
+        }return spareStack;
+    }
+
+        @Override
+        public void deleteSpare (Stack<Spare> spareStack, String reference){
+             //spareStack.stream().map(spare -> spare.getReference().equals(reference) ? spareStack.remove(spare): null);
+             spareStack.forEach(spare -> {
+                 if (spare.getReference().equals(reference)){
+                     System.out.println("Eliminando...");
+                     spareStack.remove(spare);
+                 }
+             });
         }
-        menu(spareStack);
+
+        @Override
+        public Spare sellSpare (Stack < Spare > spareStack, String reference, Long units){
+            spareStack.forEach(spare -> {
+                if (spare.getReference().equals(reference)){
+                    System.out.println("vendiendo...");
+                    if (spare.getCuantity() < units){
+                        System.out.println("No se puede vender el producto debido a que no hay cantidad suficiente. Cantidad actual: "+ spare.getCuantity());
+                    }else{
+                        System.out.println("Vendido "+ spare.getReference());
+                        spare.setCuantity(spare.getCuantity() - units);
+                    }
+                }
+            });
+            return null;
+        }
     }
 
-    public void menu (Stack<Spare> spareStack){
-
-    }
-
-    @Override
-    public Spare findSpare(Stack<Spare> spareStack, String reference) {
-        return spareStack.stream()
-                .map(spare-> spare.getReference().equals(reference) ? spare : null)
-                .filter(Objects::nonNull)
-                .findFirst()
-                .orElse(null);
-
-    }
-
-    @Override
-    public Spare addSpare(Stack<Spare> spareStack, Spare spare) {
-        return null;
-    }
-
-    @Override
-    public void createSpare(String brand, String reference, Long cuantity, BigDecimal price) {
-
-    }
-
-    @Override
-    public Spare deleteSpare(Stack<Spare> spareStack, String reference) {
-        return null;
-    }
-
-    @Override
-    public Spare sellSpare(Stack<Spare> spareStack, String reference, Long units) {
-        return null;
-    }
-}
